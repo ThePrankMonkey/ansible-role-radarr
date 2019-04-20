68c90744d151d6fdb23639021ddda8f6c4df03e2
Matthew Hellmer <matthew.hellmer@ntrepidcorp.com>
2019-04-20 00:51:02 -0400

    Correction for unarchive and enhanced tests.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_hosts_file(host):
-    f = host.file('/etc/hosts')
+def test_radarr_service(host):
+    host.service('radarr').is_running
 
-    assert f.exists
-    assert f.user == 'root'
-    assert f.group == 'root'
+
+def test_radarr_page(host):
+    cmd = host.run("curl 127.0.0.1:7878")
+    cmd.rc == 0
+    "Radarr" in cmd.stdout
